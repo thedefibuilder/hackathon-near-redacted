65bc5859683510d82ab7573025e693f7907757df
AsteroNEO <5530388+Lem0nTree@users.noreply.github.com>
2024-10-23 22:35:53 +0200

    Improved skelethon

--- app/src/components/investments-plan/invetsments-plan-header-card.tsx
+++ app/src/components/investments-plan/invetsments-plan-header-card.tsx
@@ -85,7 +85,7 @@ export default function InvestmentPlanHeaderCard({
 
       <div className="h-4" />
       <div className="flex items-center gap-3">
-        <div className="rounded-full bg-primary px-6 py-2 text-black">
+        <div className="rounded-full bg-primary px-6 py-2 font-semibold text-black">
           Invest Now
         </div>
 
